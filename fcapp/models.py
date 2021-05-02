6dc8225db0ec91ceb6e5be4d8e5cb1db8f1f2797
Xin Li <lx5050530@gmail.com>
2021-05-02 15:34:40 -0500

    change class name
    change the class name to FoodCompanion

--- fcapp/models.py
+++ fcapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class FoodProducts(models.Model):
+class FoodCompanion(models.Model):
     product = models.TextField(max_length = 255)
     calories = models.IntegerField()
     ingredients = models.TextField(max_length = 500)
@@ -15,20 +15,20 @@ class FoodProducts(models.Model):
     halal = models.BooleanField()
     kosher = models.BooleanField()
 
-from models import FoodProducts
+from models import FoodCompanion
 from django.db.models import Q
 
 # 1. query all items form foodproducts where country = 'United States'
-country = FoodProducts.objects.filter(country__iexact = 'United States')
+country = FoodCompanion.objects.filter(country__iexact = 'United States')
 print(country)
 # 2. all the ones from Wholefood
-store = FoodProducts.objects.filter(store__iexact = 'Wholefoods')
+store = FoodCompanion.objects.filter(store__iexact = 'Wholefoods')
 print(store)
 # 3. all the ones with less than 300 calories
-calories = FoodProducts.objects.filter(calories__lt < 300)
+calories = FoodCompanion.objects.filter(calories__lt < 300)
 print(calories)
 # 4. all the ones listed as Vegan
-vegan = FoodProducts.objects.filter(vagan__iexact = 'True')
+vegan = FoodCompanion.objects.filter(vagan__iexact = 'True')
 print(vegan)
 # 5. all the ones Vegetarian
-vegetarian = FoodProducts.objects.filter(Q(vagan__iexact = 'True') | Q(vegetarian__iexact = 'True'))
+vegetarian = FoodCompanion.objects.filter(Q(vagan__iexact = 'True') | Q(vegetarian__iexact = 'True'))
